15a0b92f0a5b6fe1f149515d77e685598102c25b
Adetomokun Israel <90700572+Israel-sanmi@users.noreply.github.com>
2022-02-12 01:02:37 +0100

    Add files via upload

---
+++ layouts/script.js
@@ -0,0 +1,11 @@
+const dropFunct = function(){
+const button = document.getElementById('btn');
+const menu = document.getElementById('menu');
+
+button.addEventListener('click', () =>{
+    menu.classList.toggle('hidden')
+})
+}
+dropFunct();
+
+
